2210b4d5bb9e93a2e5101fc97507477accb02402
rgloginja <robertgloginja@noisediary.com>
2016-03-04 17:42:52 +1100

    Update menu.lua
    Changes to include the Start Button and its font (flashing included)

--- menu.lua
+++ menu.lua
@@ -16,12 +16,11 @@ local x
 
 local BackgroundImage
 
+local backgroundMusic
+local backgroundMusicChannel
+
 local StartButton
 
-local function loadGame( event )
-    -- body
-    composer.gotoScene( "mainGame" )
-end
 function scene:create( event )
     local sceneGroup = self.view
 
@@ -29,17 +28,42 @@ function scene:create( event )
     -- 
     -- INSERT code here to initialize the scene
     -- e.g. add display objects to 'sceneGroup', add touch listeners, etc
-    
+    local function loadGame( event )
+        -- body
+        local options = {
+            effect = "fade",
+            time = 1000
+        }
+
+        composer.gotoScene( "mainGame", options )
+
+    end
+
+    backgroundMusic = audio.loadSound( "bg2.ogg" )
+    backgroundMusicChannel = audio.play( backgroundMusic, { channel=1, loops=-1, fadein=50 } )
+
     x=display.contentWidth
 
     BackgroundImage = display.newImage( "bg2.jpg", true )
     BackgroundImage.x = display.contentWidth / 2
     BackgroundImage.y = display.contentHeight / 2
+    BackgroundImage.height = display.contentHeight
+    BackgroundImage.width = display.contentWidth * 1.5
     sceneGroup:insert(BackgroundImage)
 
-    StartButton = display.newText("Start", display.contentWidth/2,display.contentHeight/2)
+    StartButton = display.newText("Click To Start", display.contentWidth/2,display.contentHeight/2,'blowbrush',32)
+    StartButton:setFillColor(0)
     StartButton:addEventListener("touch", loadGame)
     sceneGroup:insert(StartButton)
+
+    function function1(e)
+        transition.to(StartButton,{time=800,alpha=1, onComplete=function2})
+    end
+
+    function function2(e)
+        transition.to(StartButton,{time=800,alpha=0, onComplete=function1})
+    end
+    transition.to(StartButton,{time=800,alpha=0.2, onComplete = function1})
 
 end
 
@@ -50,7 +74,7 @@ function scene:show( event )
     if phase == "will" then
        
     elseif phase == "did" then
-       composer.removeScene("mainGame");
+       --composer.removeScene("mainGame");
     end 
 end
 
@@ -65,6 +89,7 @@ function scene:hide( event )
         -- e.g. stop timers, stop animation, unload sounds, etc.)
     elseif phase == "did" then
         -- Called when the scene is now off screen
+        --composer.removeScene("menu")
 		
     end 
 end
